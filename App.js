2db2244289faaca55f613a020d866eeba7be1a5c
ByeongHunKim <rkwhr912@naver.com>
2022-04-24 21:01:33 +0900

    #2.6 Styles part Two (ScrollView, Dimensions, pagingEnabled)

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+// ES6사용 === const SCREEN_WIDTH = Dimensions.get("window").width;
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
@@ -8,12 +11,17 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        horizontal
+        style={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -32,11 +40,9 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: '500',
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: 'center',
   },
   temp: {
