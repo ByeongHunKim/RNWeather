0c160600464676cbc8d94320e715e524df7f4c71
ByeongHunKim <rkwhr912@naver.com>
2022-04-25 14:07:00 +0900

    #2.10 Icons

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
 
 // ES6사용 === const SCREEN_WIDTH = Dimensions.get("window").width;
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 const API_KEY = 'bd093f06d918ed4f87f1396a260737b8';
+
+const icons = {
+  Clouds: 'cloudy',
+  Clear: 'day-sunny',
+  Rain: 'rains',
+  Atmosphere: 'cloudy-gusts',
+  Snow: 'snowflake',
+  Drizzle: 'rain',
+  ThunderStorm: 'lightning',
+};
 
 export default function App() {
   const [city, setCity] = useState('Loading...');
@@ -51,13 +62,23 @@ export default function App() {
         style={styles.weather}
       >
         {days.length === 0 ? (
-          <View style={styles.day}>
+          <View style={{ ...styles.day, alignItems: 'center' }}>
             <ActivityIndicator style={{ marginTop: 10 }} color="white" size="large" />
           </View>
         ) : (
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  width: '100%',
+                }}
+              >
+                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <Fontisto name={icons[day.weather[0].main]} size={68} color="white" />
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
@@ -81,21 +102,29 @@ const styles = StyleSheet.create({
   cityName: {
     fontSize: 68,
     fontWeight: '500',
+    color: 'white',
   },
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    paddingHorizontal: 20,
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 100,
+    color: 'white',
   },
   description: {
-    marginTop: -30,
-    fontSize: 100,
+    marginTop: -10,
+    fontSize: 30,
+    color: 'white',
+    fontWeight: '500',
   },
   tinyText: {
-    fontSize: 20,
+    fontSize: 25,
+    marginTop: -5,
+    color: 'white',
+    fontWeight: '500',
   },
 });
